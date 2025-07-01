37b4e68ddbc6cd30121476c97ce3a0a721f60089
sushma <sushma.pal@ciphersquare.tech>
2025-07-01 17:44:08 +0530

    updated

--- src/app/pages/staff-management/staff-management.routes.ts
+++ src/app/pages/staff-management/staff-management.routes.ts
@@ -1,46 +1,45 @@
-
-import { StaffManagementComponent } from "./staff-management.component";
-import { StaffListComponent } from "./staff-list/staff-list.component";
-import { AddStaffComponent } from "./add-staff/add-staff.component";
-import { RoleListComponent } from "./role-management/role-list/role-list.component";
-import { RoleCreateComponent } from "./role-management/role-create/role-create.component";
-import { PermissionListComponent } from "./permission-management/permission-list/permission-list.component";
+import { StaffManagementComponent } from './staff-management.component';
+import { StaffListComponent } from './staff-list/staff-list.component';
+import { AddStaffComponent } from './add-staff/add-staff.component';
+import { RoleListComponent } from './role-management/role-list/role-list.component';
+import { RoleCreateComponent } from './role-management/role-create/role-create.component';
+import { PermissionListComponent } from './permission-management/permission-list/permission-list.component';
 
 export const staffManagementRoutes = [
-    {
-        path: '',
-        children: [
-            {
-                path: 'role-list',
-                component: RoleListComponent
-            },
-            {
-                path: 'add',
-                component: RoleCreateComponent
-            },
-            {
-                path: 'role-edit',
-                component: RoleCreateComponent,
-                data: { edit: true, role: 'role-update' }
-            },
-            {
-                path: 'permissions',
-                component: PermissionListComponent
-            },
-            {
-                path: 'staff-list',
-                component: StaffListComponent
-            },
-            {
-                path: 'add-staff',
-                component: AddStaffComponent
-            },
-            {
-                path: 'edit-staff/:id',
-                component: AddStaffComponent,
-            },
-        ],
-        component: StaffManagementComponent
-    }
-
+  {
+    path: '',
+    children: [
+      {
+        path: 'role-list',
+        component: RoleListComponent,
+      },
+      {
+        path: 'add',
+        component: RoleCreateComponent,
+      },
+      {
+        path: 'role-edit',
+        component: RoleCreateComponent,
+        data: { edit: true, role: 'role-update' },
+      },
+      {
+        path: 'permissions',
+        component: PermissionListComponent,
+        data: { page: 'permission' },
+      },
+      {
+        path: 'staff-list',
+        component: StaffListComponent,
+      },
+      {
+        path: 'add-staff',
+        component: AddStaffComponent,
+      },
+      {
+        path: 'edit-staff/:id',
+        component: AddStaffComponent,
+      },
+    ],
+    component: StaffManagementComponent,
+  },
 ];
