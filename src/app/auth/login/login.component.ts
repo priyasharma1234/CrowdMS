75625cf954b2dfdbee5bddd9144d8afb76f1aa6e
priya <priya.sharma@ciphersquare.tech>
2025-07-21 18:20:21 +0530

    Changes in login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {Router, RouterLink} from '@angular/router';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {ILoginApiPayload, ILoginApiResponse} from './login.types';
-import {ApiRequestService} from '../../services/api-request.service';
-import {apiRoutes} from '../../config/api-request';
-import {AuthCoreService} from '../../services/auth-core.service';
+import { Router, RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ILoginApiPayload, ILoginApiResponse } from './login.types';
+import { ApiRequestService } from '../../services/api-request.service';
+import { apiRoutes } from '../../config/api-request';
+import { AuthCoreService } from '../../services/auth-core.service';
+import { NgxToasterService } from 'src/app/core/services/toasterNgs.service';
 
 @Component({
   selector: 'app-login',
@@ -25,7 +26,8 @@ export class LoginComponent {
     private _formBuilder: FormBuilder,
     private _ApiRequestService: ApiRequestService,
     private _AuthCoreService: AuthCoreService,
-    private _Router: Router
+    private _Router: Router,
+    private _NgxToasterService: NgxToasterService
   ) {
     this.loginForm = this._formBuilder.group({
       userId: [''],
@@ -34,17 +36,18 @@ export class LoginComponent {
   }
 
   Login() {
-    if (!this.loginForm.valid) {
-      console.error('Form is invalid');
-      return;
+    this.loginForm.markAllAsTouched()
+    if (this.loginForm.valid) {
+      const payload: ILoginApiPayload = { username: this.loginForm.value.userId ?? "", password: this.loginForm.value.password ?? "" };
+      this._ApiRequestService.postData<ILoginApiPayload, ILoginApiResponse>({ payload: payload }, apiRoutes.auth.login).subscribe(res => {
+        if (res.statuscode == 200) {
+          this._AuthCoreService.SetUser(res.data.user, res.data.token);
+          this._Router.navigate(['/dashboard']);
+        } else {
+          this._NgxToasterService.showError(res?.message ?? res['data'][0], 'Error');
+        }
+      });
     }
-    const payload: ILoginApiPayload = {username: this.loginForm.value.userId ?? "", password: this.loginForm.value.password ?? ""};
-    this._ApiRequestService.postData<ILoginApiPayload, ILoginApiResponse>({payload: payload}, apiRoutes.auth.login).subscribe(res => {
-      if(res.statuscode == 200) {
-        this._AuthCoreService.SetUser(res.data.user, res.data.token);
-        this._Router.navigate(['/dashboard']);
-      }
-    });
 
   }
 
