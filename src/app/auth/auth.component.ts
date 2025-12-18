ee97ffc4d2ce069d689ea1e2716d9daa7a3e4956
priya <priya.sharma@ciphersquare.tech>
2025-12-18 20:08:20 +0530

    Worked on auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {DynamicTableModule} from '@ciphersquare/dynamic-table';
 
 @Component({
   selector: 'app-auth',
   imports: [
     RouterOutlet,
-    DynamicTableModule,
   ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
